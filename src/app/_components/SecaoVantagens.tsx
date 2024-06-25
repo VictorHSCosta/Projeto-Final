8b6e8375d90d43678b24a97f85baa98403c3a34c
PedroDP1804 <pedropaulacampos@gmail.com>
2024-06-25 19:38:17 -0300

    Responsividade das Vantagens

--- src/app/_components/SecaoVantagens.tsx
+++ src/app/_components/SecaoVantagens.tsx
@@ -5,17 +5,17 @@ export default function() {
     return (
         <ul className={styles.vantagens}>
             <li>
-              <Vantagem content="Livros a Preço de Banana :D">
-                <img className={styles.icons_vantagens} src={"/images/icon_cartao.svg"}/>
+              <Vantagem content="Excelente relação custo-benefício!">
+                <img className={styles.icons_vantagens} src={"/images/icon_dinheiro.svg"}/>
               </Vantagem>
             </li>
             <li>
-              <Vantagem content="Aceitamos Fiado!">
-                <img className={styles.icons_vantagens} src={"/images/icon_cartao.svg"}/>
+              <Vantagem content="Livros em excelente estado!">
+                <img className={styles.icons_vantagens} src={"/images/icon_livro.svg"}/>
               </Vantagem>
             </li>
             <li>
-              <Vantagem content="Acabou a criatividade :(">
+              <Vantagem content="Coleção única, com unidades raras!">
                 <img className={styles.icon_estante} src={"/images/icon_estante.svg"}/>
               </Vantagem>
             </li>
